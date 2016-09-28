1506d5735a0856f5cf5d2d9190e361f8e7c47f48
jmaisel <joe.maisel@gmail.com>
2016-09-27 21:46:02 -0700

    experimenting

--- odroid-ubuntu64-chroot.sh
+++ odroid-ubuntu64-chroot.sh
@@ -3,6 +3,8 @@ OS_IMG=ubuntu-16.04-minimal-odroid-c2.img
 ROOTFS=$BUILD_HOME/$OS_IMG-root
 BOOTFS=$BUILD_HOME/$OS_IMG-boot
 
+DEPENDENCIES="vim git"
+
 sudo kpartx -a -v $BUILD_HOME/imgs/$OS_IMG
 
 sleep 2 
@@ -14,7 +16,8 @@ sudo mount /dev/mapper/loop0p1  $BOOTFS
 sudo mount /dev/mapper/loop0p2  $ROOTFS
 
 echo executing chroot
-sudo proot -q qemu-aarch64 -S $ROOTFS -b $BOOTFS:/boot /bin/bash
+sudo proot -q qemu-aarch64 -S $ROOTFS -b $BOOTFS:/boot uname -a 
+sudo proot -q qemu-aarch64 -S $ROOTFS -b $BOOTFS:/boot apt-get install $DEPENDENCIES 
 echo exited chroot
 
 echo unmounting kpartx loopback connectors
