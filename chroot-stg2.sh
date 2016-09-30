2128789c1f8fccbeaf827b5b76f42c347a51d983
jmaisel <joe.maisel@gmail.com>
2016-09-29 19:45:21 -0700

    latest node_projects from master install working

--- chroot-stg2.sh
+++ chroot-stg2.sh
@@ -2,21 +2,25 @@ echo
 echo 
 echo 
 echo 
-echo 
-echo 
-echo 
 echo ======================================================================================================================================================
-echo "| stage 2 executing in \"`uname -a`\" |"
+echo "| stage 2 in \"`uname -a`\" |"
 echo ======================================================================================================================================================
 echo 
 
 DEPENDENCIES="tightvncserver vim git chromium-browser"
 
+apt-get -y update
 apt-get -y install $DEPENDENCIES 
 
-echo $NODEJS_ID
 # install nodejs
+echo setting up nodejs symlinks
 ln -s /opt/joebotics/$NODEJS_ID/ /opt/joebotics/nodejs
 ln -s /opt/joebotics/nodejs/bin/node /usr/bin/node 
 ln -s /opt/joebotics/nodejs/bin/npm /usr/bin/npm
+
+echo installing odroid-gpio for nodejs
+npm install odroid-gpio
+mv node_modules/* /opt/joebotics/lib/node_modules
+rm -fr node_modules
+
 
